dd47d02d165b61e6417941429506e066dad805c8
timothy <timothy.decort@polaris-dc.com>
2020-10-28 13:30:45 +0100

    Add environment variables to config

--- config/lightspeed-ecom-api.php
+++ config/lightspeed-ecom-api.php
@@ -10,19 +10,22 @@ return [
      * "live" and "eu1" point to the same cluster. If you don't work for Lightspeed you probably won't need "local".
      * For more information check https://developers.lightspeedhq.com/ecom/introduction/clusters/
      */
-    'cluster' => 'live',
+    'cluster' => env('LS_ECOM_API_CLUSTER', 'live'),
 
     /*
      * Enter your Lightspeed API key and secret. You can request a set of credentials via the Lightspeed support service.
      */
-    'key' => '',
-    'secret' => '',
+    'key' => env('LS_ECOM_API_KEY'),
+    'secret' => env('LS_ECOM_API_SECRET'),
 
     /*
      * Enter your default shop language.
      * Possible options can be found via the "settings/internationalization" menu in your webshop backoffice.
      */
-    'language' => 'en',
+    'language' => env('LS_ECOM_API_LANGUAGE', 'en'),
 
-    'save_remaining_calls_to_cache' => env('LS_ECOM_API_SAVE_CALLS_TO_CACHE', true)
+    /*
+     * Set this to false when you don't want to cache your remaining API calls in the local cache driver.
+     */
+    'save_remaining_calls_to_cache' => env('LS_ECOM_API_SAVE_CALLS_TO_CACHE', true),
 ];
