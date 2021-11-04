cff0fcf496b6bb81424009168da7c2b3c8edbd5c
timothy <timothy.decort@polaris-dc.com>
2021-11-04 19:28:40 +0100

    fix tests

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -10,7 +10,7 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
@@ -29,6 +29,6 @@ return PhpCsFixer\Config::create()
         'method_argument_space' => [
             'on_multiline' => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
-        ]
+        ],
     ])
     ->setFinder($finder);
